d257db32a43381ad4727fc210b265ed206c83791
Alen Thomas <alen.thomas@ucdconnect.ie>
2020-02-15 21:23:39 +0000

    Update Rest

--- src/home/controllers/rest/Papillon.java
+++ src/home/controllers/rest/Papillon.java
@@ -38,5 +38,156 @@ public class Papillon {
          *  */
     }
 
+    /**
+     *
+     *
+     *
+     *
+     *              Floor
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
+    // TODO: Gets a datacenter’s all floors with hierarchical data applied
+    public void getAllFloors(int datacenterId){
+        /**
+         *  /datacenters/{datacenterId}/allfloors
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a datacenter’s all floors with hierarchical data and power usage applied
+    public void getAllFloorsPowerUsage(int datacenterId, int startTime, int endTime){
+        /**
+         *  /datacenters/{datacenterId}/allfloors/powers?starttime={startTime}&endtime={endTime}
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a datacenter’s all floors
+    public void getFloors(int datacenterId){
+        /**
+         *  /datacenters/{datacenterId}/floors
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a floor
+    public void getFloors(int datacenterId, int floorId){
+        /**
+         *  /datacenters/{datacenterId}/floors/{floorId}
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a floors’s power usage for a given time span
+    public void getFloorPowerUsage(int datacenterId, int floorId, int startTime, int endTime){
+        /**
+         *  /datacenters/{datacenterId}/floors/{floorId}/power?starttime={startTime}&endtime={endTime}
+         *
+         * Look at API reference
+         *  */
+    }
+
+
+    /**
+     *
+     *
+     *
+     *
+     *              Racks
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
+    // TODO: Gets a datacenter’s all racks with hierarchical data applied
+    public void getAllRacks(int datacenterId){
+        /**
+         *  /datacenters/{datacenterId}/allracks
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a datacenter’s all racks with hearchical data and power usage applied
+    public void getAllRacksPowerUsage(int datacenterId, int startTime, int endTime){
+        /**
+         *  /datacenters/{datacenterId}/allracks/powers?starttime={startTime}&endtime={endTime}
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a rack belonging to a floor/datacenter
+    public void getRack(int datacenterId, int floorId, int rackId){
+        /**
+         *  /datacenters/{datacenterId}/floors/{floorId}/racks /{rackId}
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets all racks belonging to a floor/datacenter
+    public void getRacks(int datacenterId, int floorId){
+        /**
+         *  /datacenters/{datacenterId}/floors/{floorId}/racks/
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a rack’s power usage for a given time span
+    public void getFloorPowerUsage(int datacenterId, int floorId, int rackId,int startTime, int endTime){
+        /**
+         *  /datacenters/{datacenterId}/floors/{floorId}/racks/{rackId}/power?starttime={startTime}&endtime={endTime}
+         *
+         * Look at API reference
+         *  */
+    }
+
+    /**
+     *
+     *
+     *
+     *
+     *              Hosts
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
+    // TODO: Gets a datacenter’s all hosts with hierarchical data applied
+    public void getAllHosts(int datacenterId){
+        /**
+         *  /datacenters/{datacenterId}/allhosts
+         *
+         * Look at API reference
+         *  */
+    }
+
+    // TODO: Gets a datacenter’s all hosts with hearchical data and power usage applied
+    public void getAllHostsPowerUsage(int datacenterId, int startTime, int endTime){
+        /**
+         *  /datacenters/{datacenterId}/allhosts/powers?starttime={startTime}&endtime={endTime}
+         *
+         * Look at API reference
+         * */
+    }
+
 
 }
