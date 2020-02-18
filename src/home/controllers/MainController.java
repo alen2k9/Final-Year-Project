d5d6ff062bd5d6c10625eb11a50f5fc775625ecc
Alen Thomas <alen.thomas@ucdconnect.ie>
2020-02-18 12:38:50 +0000

    Add tabbed anchor pane

--- src/home/controllers/MainController.java
+++ src/home/controllers/MainController.java
@@ -1,15 +1,18 @@
 package home.controllers;
 
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
 public class MainController {
 
-    public void home(MouseEvent mouseEvent) {
+    public BorderPane borderPane;
+
+    public void homeClicked(MouseEvent mouseEvent) {
     }
 
-    public void dashboard(MouseEvent mouseEvent) {
+    public void dashboardClicked(MouseEvent mouseEvent) {
     }
 
-    public void settings(MouseEvent mouseEvent) {
+    public void settingsClicked(MouseEvent mouseEvent) {
     }
 }
