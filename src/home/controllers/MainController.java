ae47be91949b769aebd72096ff47fd283e138816
Alen Thomas <alen.thomas@ucdconnect.ie>
2020-02-18 12:51:52 +0000

    Full working tabs

--- src/home/controllers/MainController.java
+++ src/home/controllers/MainController.java
@@ -1,5 +1,7 @@
 package home.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
@@ -8,11 +10,24 @@ public class MainController {
     public BorderPane borderPane;
 
     public void homeClicked(MouseEvent mouseEvent) {
+        loadUI("Home");
     }
 
     public void dashboardClicked(MouseEvent mouseEvent) {
+        loadUI("Dashboard");
     }
 
     public void settingsClicked(MouseEvent mouseEvent) {
+        loadUI("Settings");
+    }
+
+    private void loadUI(String tab){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../fxml/"+tab+".fxml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(root);
     }
 }
