d6c9395eb0c549c364d3fc0e51226c3108615922
Alen Thomas <alen.thomas@ucdconnect.ie>
2020-05-12 21:33:35 +0100

    More Unit Tests

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,16 +1,26 @@
+import data.mysql.MYSQL;
+import data.mysql.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
     @org.junit.jupiter.api.Test
-    void getPowerData() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getPowerDataDay() {
-    }
-
-    @org.junit.jupiter.api.Test
     void setUpUser() {
+        MYSQL mysql = new MYSQL();
+        List<User> users = mysql.getUsers();
+        for(User user: users){
+            assertNull(user.hosts);
+        }
+
+        for(User user: users){
+            assertFalse(user.hosts instanceof ArrayList);
+            user.setUpUser();
+            assertTrue(user.hosts instanceof ArrayList);
+        }
+
     }
 }
